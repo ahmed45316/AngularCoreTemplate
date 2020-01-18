e351b622dc711fd529ab590b939789498e22da56
mahmoud <mahmoud@DESKTOP-39NA5S9>
2020-01-18 21:11:32 +0200

    add drop down reusable component

---
+++ src/app/modules/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-drop-down',
+    templateUrl: './dropdown.component.html',
+    styleUrls: ['./dropdown.component.scss']
+  })
+  export class DropDownComponent implements OnInit {
+      constructor() {}
+
+      ngOnInit(): void {}
+  }
